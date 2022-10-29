fbed5d529100e247849837c8937ae83b1b5b042a
Stacey Kinder <76706319+StaceyKinder@users.noreply.github.com>
2022-10-29 09:23:25 +0300

    Update Credits.js

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -3,19 +3,10 @@ import React from 'react';
 export default function Credits() {
   return (
     <div className="credits">
-      <div className="me">
-        <p>
-          made by <a href="https://twitter.com/iamtekeste">@iamtekeste</a>
-        </p>
-        <a
-          className="twitter-share-button"
-          href="https://twitter.com/intent/tweet?url=https://typehero.now.sh/&text=Try Google fonts on beautiful backgrounds & colors&via=iamtekeste"
-        >
-          Tweet
-        </a>
-        <a className="twitter-follow-button" href="https://twitter.com/iamtekeste" data-show-screen-name="false" data-show-count="false">Follow @iamtekeste</a>
+      <div className="me">        
       </div>
       <p>Search powered by <a href="https://www.algolia.com/">Algolia</a></p>
+      <p><a href="https://fxstrengthmeter.com/" title="CSM Tool"><img src="https://fxstrengthmeter.com/fxsm-logo.png" alt="Currency Strength Meter" width="120" height="30"></a></p>
     </div>
   );
 }
